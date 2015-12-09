2c88ab51dddb7db8dd61b7f455dcb710bffeb093
Nicco Kunzmann <niccokunzmann@rambler.ru>
2015-12-09 14:08:29 -0800

    added push saving code

--- docker/ubuntu/build/06_push.sh
+++ docker/ubuntu/build/06_push.sh
@@ -3,6 +3,9 @@
 # go to the docker directory
 cd "`dirname $0`"/../../docker
 
+scripts/run zen setup/06_save_directory_for_push.sh && \
+scripts/commit_all "copied contents of cp-local-development"
+
 docker push coderdojo/zen
 docker push coderdojo/postgres-zen
 
