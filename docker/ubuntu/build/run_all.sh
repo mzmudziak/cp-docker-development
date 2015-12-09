4f15c69d20df1e9a514322c56ef017d4ce928c86
Nicco Kunzmann <niccokunzmann@rambler.ru>
2015-12-09 11:36:53 -0800

    modified run_all.sh accordingly

--- docker/ubuntu/build/run_all.sh
+++ docker/ubuntu/build/run_all.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-( ./00_remove_old_images.sh && ./01_build.sh && ./02_install.sh && ./03_initialize.sh && ./04_run.sh ) | tee run_all.log
+( 
+  ./00_remove_old_images.sh
+  ./01_build.sh && ./02_install.sh && ./03_initialize.sh && ./04_testdata.sh && ./05_run.sh
+) | tee run_all.log
+
+echo 
+echo "The output has beed written to run_all.log"
