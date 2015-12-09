eee5ce28d7136e493866e9db4e1beb9a0ebbb441
Nicco Kunzmann <niccokunzmann@rambler.ru>
2015-12-09 01:28:19 -0800

    added hostname and port to local-en

--- docker/docker/zen/setup/local-env.js
+++ docker/docker/zen/setup/local-env.js
@@ -2,7 +2,9 @@ module.exports = {
   POSTGRES_USERNAME : "platform",
   POSTGRES_PASSWORD : "QdYx3D5y",
   POSTGRES_HOST : "postgres-zen",
-  POSTGRES_PORT : 5432
+  POSTGRES_PORT : 5432,
+  PORT : 8000,
+  HOSTNAME : "zen"
 };
 
 console.log("test local-env.js");
