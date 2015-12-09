8ff9923f6c41b2e142b08f16d2ab19060a7a2c9b
Nicco Kunzmann <niccokunzmann@rambler.ru>
2015-12-09 12:16:58 -0800

    made scripts executable

--- docker/ubuntu/develop/run_zen_bash.sh
+++ docker/ubuntu/develop/run_zen_bash.sh
@@ -2,5 +2,6 @@
 
 cd `dirname $0`/../build
 
+echo "exit the command line with the command \"exit\". Otherwise you loose work that is not in the code directory."
 ./scripts/run zen setup/05_bash.sh
 ./scripts/commit_all
