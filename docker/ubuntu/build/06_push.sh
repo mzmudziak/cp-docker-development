1ff7aedace90b6c429954d96ae3fb0aa1d2a1d4b
Nicco Kunzmann <niccokunzmann@rambler.ru>
2015-12-10 06:40:41 -0800

    added push abort when commit fails

--- docker/ubuntu/build/06_push.sh
+++ docker/ubuntu/build/06_push.sh
@@ -3,7 +3,7 @@
 cd "`dirname $0`"
 
 scripts/run zen setup/06_save_directory_for_push.sh && \
-scripts/commit_all "copied contents of cp-local-development"
+scripts/commit_all "copied contents of cp-local-development" || { echo "ERROR: could not create commit." ; exit 1 ; }
 
 docker login
 
