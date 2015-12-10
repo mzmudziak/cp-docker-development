5d556d13525f3c0489677f7505b3fab0221f46af
Nicco Kunzmann <niccokunzmann@rambler.ru>
2015-12-10 10:54:05 -0800

    moved docker login to the start so it dows not interupt in the middle

--- docker/ubuntu/build/06_push.sh
+++ docker/ubuntu/build/06_push.sh
@@ -2,10 +2,10 @@
 
 cd "`dirname $0`"
 
+docker login
+
 scripts/run zen setup/06_save_directory_for_push.sh && \
 scripts/commit_all "copied contents of cp-local-development" || { echo "ERROR: could not create commit." ; exit 1 ; }
-
-docker login
 
 docker push coderdojo/zen
 docker push coderdojo/postgres-zen
