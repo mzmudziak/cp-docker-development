50fa99e562771a2b1e0ea5742d3bb6d6abaeeaa1
Nicco Kunzmann <niccokunzmann@rambler.ru>
2015-12-09 16:11:29 -0800

    added test for cp-local-development not existing

--- docker/docker/zen/setup/config.sh
+++ docker/docker/zen/setup/config.sh
@@ -7,7 +7,7 @@ clone_url="https://github.com/CoderDojo/cp-local-development.git"
 directory=/user/src/app/cp-local-development
 code_copy=$directory/../code_copy
 
-if ! [ "`ls -A $directory`" ]
+if ! [ -d "$directory" ] || ! [ "`ls -A $directory`" ]
 then
   # $directory is empty
   if [ -d "$code_copy" ] && [ "`ls -A "$code_copy"`" ]
