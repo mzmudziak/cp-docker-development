eb34a5c36e82e49443291e95e2f14f6b43765d68
Nicco Kunzmann <niccokunzmann@rambler.ru>
2015-12-10 11:04:01 -0800

    added parameters to bash

--- docker/ubuntu/develop/run_zen_bash.sh
+++ docker/ubuntu/develop/run_zen_bash.sh
@@ -3,5 +3,5 @@
 cd `dirname $0`/../build
 
 echo "exit the command line with the command \"exit\". Otherwise you loose work that is not in the code directory."
-./scripts/run zen setup/05_bash.sh
+./scripts/run zen setup/05_bash.sh "$@"
 ./scripts/commit_all
