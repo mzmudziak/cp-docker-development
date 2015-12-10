8b62f6fe3e9b67ab3a998080627cde0db8c84657
Nicco Kunzmann <niccokunzmann@rambler.ru>
2015-12-09 16:28:14 -0800

    made patch mandatory

--- docker/ubuntu/build/02_install.sh
+++ docker/ubuntu/build/02_install.sh
@@ -2,10 +2,19 @@
 
 cd `dirname $0`
 
-scripts/run zen setup/01_install.sh
-success=$?
-patch -p1 -N ../../code/system.js ./diffs/system.js.diff
+if scripts/run zen setup/01_install.sh
+then
+  scripts/commit_all "installation complete"
+else
+  exit 1
+fi
 
-( exit $success ; ) && scripts/commit_all "installation complete"
+diff="`pwd`/diffs/system.js.diff"
+
+cd ../../code/
+
+sudo chown ${USER} . -R
+
+patch -p1 -N system.js $diff || { echo "Could not set POSTGRES_HOST in code/system.js to \"postgres-zen\". You need to do it manually or the 03_initialize.sh fails because it can not connect to postgres." ; exit 1 ; }
 
 
