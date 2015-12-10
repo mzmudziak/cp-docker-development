8417912d7153d50f775faa8f0be1cb0e32b414c2
Nicco Kunzmann <niccokunzmann@rambler.ru>
2015-12-10 11:37:39 -0800

    grabbing sudo privileges

--- docker/ubuntu/build/run_all.sh
+++ docker/ubuntu/build/run_all.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 ( 
+  # grab sudo priviledges so we do not need to interrupt the following process if they are needed
+  sudo true
+  # execute the steps
   ./00_remove_old_images.sh
   ./01_build.sh && ./02_install.sh && ./03_initialize.sh && ./04_testdata.sh && ./05_run.sh
 ) | tee run_all.log
