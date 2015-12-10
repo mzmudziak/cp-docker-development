4057074eba8fd95e037f41eb37ced5569fb08c35
Nicco Kunzmann <niccokunzmann@rambler.ru>
2015-12-10 12:41:09 -0800

    also removing docker containers when removing images

--- docker/ubuntu/remove_all_docker_images.sh
+++ docker/ubuntu/remove_all_docker_images.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+cd "`dirname $0`"
+
+./build/scripts/stop_all
+
+for container_name in `./build/scripts/container_names`
+do
+  docker rm $container_name
+done
+
 docker images | awk '{print $3}' | xargs --no-run-if-empty docker rmi -f
+
