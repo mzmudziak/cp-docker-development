130464ee6d2f427c4dc4eaf6bebe7df08629c29a
Nicco Kunzmann <niccokunzmann@rambler.ru>
2015-12-09 16:28:31 -0800

    added docker login

--- docker/ubuntu/build/06_push.sh
+++ docker/ubuntu/build/06_push.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# go to the docker directory
-cd "`dirname $0`"/../../docker
+cd "`dirname $0`"
 
 scripts/run zen setup/06_save_directory_for_push.sh && \
 scripts/commit_all "copied contents of cp-local-development"
+
+docker login
 
 docker push coderdojo/zen
 docker push coderdojo/postgres-zen
