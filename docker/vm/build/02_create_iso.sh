0de3e79f8a3755766acb1fa1f911884a54704859
Nicco Kunzmann <niccokunzmann@rambler.ru>
2015-12-19 13:30:24 -0800

    added boot2zen iso creation output

--- docker/vm/build/02_create_iso.sh
+++ docker/vm/build/02_create_iso.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-docker run --rm coderdojo/boot2zen > boot2zen.iso
+if docker run --rm coderdojo/boot2zen > boot2zen.iso 
+then
+  echo "created boot2zen.iso"
+  exit 0
+else
+  echo "failed to create boot2zen.iso"
+  rm boot2zen.iso
+  exit 1
+fi
